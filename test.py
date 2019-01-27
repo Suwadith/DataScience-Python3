9ba6a0896e74eb1cab410dc6c52ee637b68e6942
Suwadith <suwadith.srithar@gmail.com>
2019-01-27 16:24:06 +0530

    end of running python scripts

---
+++ test.py
@@ -0,0 +1,9 @@
+listOfNumbers = [1, 2, 3, 4, 5, 6]
+
+for number in listOfNumbers:
+    if (number % 2 == 0):
+        print(number, "is even")
+    else:
+        print(number, "is odd")
+        
+print ("Great it works.")
